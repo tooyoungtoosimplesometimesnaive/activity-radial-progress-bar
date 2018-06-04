a2c53cdcc8b7d94bf4bfe6bf7280d288632b233c
Xing <xingmarc@yeah.net>
2018-06-03 23:17:52 -0700

    Initial partially done commit

---
+++ index.js
@@ -0,0 +1,8 @@
+
+const activityWrapper = document.getElementById('activity-wrapper')
+
+const a = document.querySelector('.circle .fill')
+const rotation = Math.floor(Math.random() * 360)
+
+// a.style.transform = 'rotate(' + rotation + 'deg)';
+
